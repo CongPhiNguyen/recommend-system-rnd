23557b36cf74113de0c15bde93a7968e4de0e06b
CongPhiNguyen <congphinguyen312@gmail.com>
2023-02-05 16:29:17 +0700

    crawl manga of mangadex

--- crawler/crawl_manga.js
+++ crawler/crawl_manga.js
@@ -38,25 +38,53 @@ const fs = require("fs").promises
   //create a new in headless chrome
   const page = await browser.newPage()
 
-  //go to target website
-  await page.goto(
-    "https://mangadex.org/titles?page=1&include=391b0order=relevance.desc",
-    {
+  for (let indexPage = 10; indexPage <= 331; indexPage++) {
+    //go to target website
+    await page.goto(`https://mangadex.org/titles?page=${indexPage}`, {
       //wait for content to load
       waitUntil: "networkidle0"
-    }
-  )
+    })
+    const manga = []
 
-  //get full page html
-  const html = await page.content()
-  console.log($)
-  $(".manga-card ").each((index, el) => {
-    const job = $(el).find("span font-bold title").text()
-    console.log(job)
-  })
+    //get full page html
+    const html = await page.content()
+    const $ = cheerio.load(html)
+    $(".manga-card").each(async (index, el) => {
+      const name = $(el).find("a.font-bold.title").text()
+      const avatar = $(el).find("img.rounded").prop("src")
+      const country = $(el).find("img.inline-block").prop("title")
+      const stat = $(el).find(".stat.group").text().trim()
+      const status = $(el).find(".tag.lift.dot").text().trim()
+      const tags = []
+      $(el)
+        .find(".tags-row.tags")
+        .each((index, val) => {
+          tags.push(
+            $(val)
+              .find(".tag.bg-accent-lighten2")
+              .text()
+              .trim()
+              .split("  ")
+              .filter((val) => val.length > 0 && val != "\n")
+              .map((val) => val.replace("\n", ""))
+              .join(",")
+          )
+        })
+      const nutshell = $(el).find(".md-md-container.dense").text().trim()
+      manga.push({
+        name,
+        avatar,
+        country,
+        stat,
+        status,
+        tags,
+        nutshell
+      })
+    })
 
-  //store html content in the reactstorefront file
-  // await fs.writeFile("test_craw_react.html", html)
+    //store html content in the reactstorefront file
+    await fs.writeFile(`value-${indexPage}.json`, JSON.stringify(manga))
+  }
 
   //close headless chrome
   await browser.close()
